dda4d5f4ea3206f2a3d4c2c75dcc7ac5888fbb9e
LAKESIDE-X <joychifunbi@yahoo.com>
2026-04-05 23:35:00 +0100

    edit

--- utils/email.go
+++ utils/email.go
@@ -7,26 +7,31 @@ import (
 )
 
 func SendEmail(toEmail string, subject string, body string) error {
-	// TEMPORARILY HARDCODE THESE TWO LINES:
-	from := "a73610001@smtp-brevo.com"
-	password := "bskE4XxANYzfMIc"
+	// 1. The credentials used to LOG IN to Brevo (Pulled safely from Render)
+	smtpUsername := os.Getenv("SMTP_EMAIL")    // e.g., a73610001@smtp-brevo.com
+	smtpPassword := os.Getenv("SMTP_PASSWORD") // Your secret Brevo Key
+	smtpHost := os.Getenv("SMTP_HOST")         // smtp-relay.brevo.com
+	smtpPort := os.Getenv("SMTP_PORT")         // 2525
 
-	smtpHost := os.Getenv("SMTP_HOST")
-	smtpPort := os.Getenv("SMTP_PORT")
+	// 2. The actual email address the message is SENT FROM (Must be verified in Brevo!)
+	// If you used a different email to sign up for Brevo, change this string!
+	senderEmail := "bamidelefelix69@gmail.com"
 
-	// The message format requires these headers so Gmail reads it correctly
+	// 3. Construct the email message (Notice we added the "From:" header)
 	message := []byte(fmt.Sprintf("To: %s\r\n"+
+		"From: %s\r\n"+
 		"Subject: %s\r\n"+
 		"\r\n"+
-		"%s\r\n", toEmail, subject, body))
+		"%s\r\n", toEmail, senderEmail, subject, body))
 
-	// Authenticate with Google
-	auth := smtp.PlainAuth("", from, password, smtpHost)
+	// 4. Authenticate using the SMTP Username (NOT the sender email)
+	auth := smtp.PlainAuth("", smtpUsername, smtpPassword, smtpHost)
 
-	// Send the email
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{toEmail}, message)
+	// 5. Send the email using the senderEmail for the envelope
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, senderEmail, []string{toEmail}, message)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
