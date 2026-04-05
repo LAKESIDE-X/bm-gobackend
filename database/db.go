a93a121dcaf49c6c36e677e666d13d96865be686
LAKESIDE-X <joychifunbi@yahoo.com>
2026-04-05 20:36:12 +0100

    edit

--- database/db.go
+++ database/db.go
@@ -14,15 +14,22 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+	// 1. First, try to grab the full URL (This is what Render uses)
+	dsn := os.Getenv("DB_URL")
 
+	// 2. If DB_URL is empty, we must be testing locally, so build it manually
+	if dsn == "" {
+		dsn = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+		)
+	}
+
+	// 3. Connect to the database
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database! \n", err)
@@ -30,7 +37,7 @@ func ConnectDB() {
 
 	log.Println("Database connection successfully opened!")
 
-	// This automatically creates the tables in pgAdmin if they don't exist
+	// 4. Run Migrations
 	log.Println("Running Database Migrations...")
 	err = db.AutoMigrate(&models.User{}, &models.Product{}, &models.CartItem{}, &models.Order{}, &models.OrderItem{}, &models.Review{}, &models.OTP{})
 	if err != nil {
